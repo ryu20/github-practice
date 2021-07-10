b5f11a75f3304d404f190e22af3b4eb707c1645a
ryu20 <gengo@qqq.to>
2021-07-10 21:54:58 +0900

    ファイル名変更委

--- Study_machine_learning/predict.py
+++ Study_machine_learning/predict.py
@@ -1,44 +1,17 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Activation, Dense
-from tensorflow.keras.layers import LSTM
-from tensorflow.keras.layers import Dropout
+from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 from getdata import prepare_data
 
 
-def build_model(x_train):
-    model = Sequential()
-    model.add(LSTM(256, input_shape=(x_train.shape[1], x_train.shape[2])))
-    model.add(Dropout(0.288662535902546))
-    model.add(Dense(1))
-    model.add(Activation("linear"))
-    model.compile(loss="mean_squared_error", optimizer='adam', metrics='mae')
-    return model
-
-
-def train_model():
+def predict_plt(model_name):
     x_train, y_train, x_test, y_test = prepare_data()
-    model = build_model(x_train)
-    history = model.fit(x_train, y_train,
-                        batch_size=200,
-                        epochs=10,
-                        verbose=1,
-                        validation_split=0.2,
-                        )
-    val_loss, val_acc = model.evaluate(x_test, y_test, verbose=0)
-    print(model.summary())
-    print("val_loss: ", val_loss)
-    print("val_acc: ", val_acc)
-    return history
+    model = load_model(f'saved_model/{model_name}')
+    pred_data = model.predict(x_train)
+    plt.plot(y_train.reshape(pred_data.shape[0]), label='train')
+    plt.plot(pred_data, label='pred')
+    plt.legend(loc='upper left')
+    plt.show()
 
 
 if __name__ == "__main__":
-    history = train_model()
-    # MAEをプロットしてみよう
-    fig, ax1 = plt.subplots(1, 1)
-
-    ax1.plot(history.epoch, history.history['loss'])
-    ax1.set_title('TrainingError')
-    ax1.set_ylabel('Model Loss', fontsize=12)
-    ax1.set_xlabel('# Epochs', fontsize=12)
-    plt.show()
+    predict_plt('hyperas_model')
---
+++ Study_machine_learning/train.py
@@ -0,0 +1,53 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import Activation, Dense
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dropout
+import matplotlib.pyplot as plt
+from getdata import prepare_data
+
+
+def build_model(x_train):
+    model = Sequential()
+    model.add(LSTM(256, input_shape=(x_train.shape[1], x_train.shape[2])))
+    model.add(Dropout(0.3))
+    model.add(Dense(1))
+    model.add(Activation("linear"))
+    model.compile(loss="mean_squared_error", optimizer='adam', metrics='mae')
+    return model
+
+
+def train_model(plt_display=False):
+    x_train, y_train, x_test, y_test = prepare_data()
+    model = build_model(x_train)
+    history = model.fit(x_train, y_train,
+                        batch_size=200,
+                        epochs=10,
+                        verbose=1,
+                        validation_split=0.2,
+                        )
+    val_loss, val_acc = model.evaluate(x_test, y_test, verbose=0)
+    print(model.summary())
+    print("val_loss: ", val_loss)
+    print("val_acc: ", val_acc)
+    model.save('saved_model/my_model')
+
+    if plt_display:
+        plt_model(history)
+
+    return
+
+
+def plt_model(history):
+    # MAEをプロットしてみよう
+    fig, ax1 = plt.subplots(1, 1)
+
+    ax1.plot(history.epoch, history.history['loss'])
+    ax1.set_title('TrainingError')
+    ax1.set_ylabel('Model Loss', fontsize=12)
+    ax1.set_xlabel('# Epochs', fontsize=12)
+    plt.show()
+
+
+if __name__ == "__main__":
+    train_model()
